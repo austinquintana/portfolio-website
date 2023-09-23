d1f7dc79975b13d686f0313760e1537e815868fa
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-09-22 23:32:23 -0600

    Adding new CSS styles

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const currentPageUrl = window.location.pathname;
+  
+//     // Select all the menu items
+//     const menuItems = document.querySelectorAll('.menuitem');
+  
+//     // Loop through the menu items and check if their href matches the current page URL
+//     menuItems.forEach(item => {
+//       const itemUrl = item.getAttribute('href');
+//       if (currentPageUrl === itemUrl) {
+//         item.classList.add('active');
+//       } else {
+//         item.classList.remove('active');
+//       }
+//     });
+//   });
